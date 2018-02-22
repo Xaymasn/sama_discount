11a015cd5229d4917efa1ba95e47ddf00082f80c
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-02-22 02:26:05 +0000

    Description

--- remiseglobale/__manifest__.py
+++ remiseglobale/__manifest__.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "remiseglobale",
+    'name': "Sama Remise Globale",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Remise globale""",
 
     'description': """
-        Long description of module's purpose
+        Permet d'ajouter une remise globale sur le devis ou la facture
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Elhadji Malang Diedhiou",
+    'website': "http://www.supermalang.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Sales Management',
+    'version': '11.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale_management'],
 
     # always loaded
     'data': [
